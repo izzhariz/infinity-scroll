cbc565856efbb138f134518c1368150fc75b68a5
IzzHrz <izzharizdev@gmail.com>
2020-10-09 23:44:15 +0800

    added scroll effect to load more images

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
 const count = 10;
 const apiKey = '1av7vBWTjcX_qI3vTe5FnPhlTd97ugdaU1VuNMQ1jzQ';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded(){
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
 
 // Helper Function to set attributes on DOM Elements
 function setAttributes(element, attributes){
@@ -17,6 +30,9 @@ function setAttributes(element, attributes){
 
 // Create Elements For Links & photosArray, Add to DOM
 function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    // console.log('total images', totalImages);
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
         const item = document.createElement('a');
@@ -32,9 +48,12 @@ function displayPhotos(){
             alt: photo.alt_description,
             title: photo.alt_description
         });
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+
         item.appendChild(img);
         imageContainer.appendChild(item);
-    })
+    });
 }
 
 // Get photos from Unsplash API
@@ -47,6 +66,16 @@ async function getPhotos(){
         // Catch error here
     }
 }
+
+// Check to see if scrolling near bottom of page, Load more photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight = window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+        
+    }
+});
+
 
 // On load
 getPhotos();
